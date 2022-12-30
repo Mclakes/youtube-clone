831610f4224f517e1a49c0477a4b0e12439035be
Mclakes <lekanhayo@gmail.com>
2022-12-30 20:49:44 +0100

    VideoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from "@mui/icons-material";
 
-const VideoCard = () => {
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle,  } from '../utils/constants';
+
+const VideoCard = ({video}) => {
+  console.log(video)
   return (
     <div>VideoCard</div>
   )
