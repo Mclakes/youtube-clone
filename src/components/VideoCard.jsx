768157503e8211097d7de2383a2320ed91ac7d43
Mclakes <lekanhayo@gmail.com>
2022-12-31 21:19:11 +0100

    finding solution

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,8 +5,9 @@ import { CheckCircle } from "@mui/icons-material";
 
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle,  } from '../utils/constants';
 
-const VideoCard = ({video}) => {
-  console.log(video)
+const VideoCard = ({video: { id: { videoId }, snippet }}) => {
+  console.log(videoId, snippet)
+  // console.log(video)
   return (
     <div>VideoCard</div>
   )
