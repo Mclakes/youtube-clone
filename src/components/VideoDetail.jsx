3071ca9f4c106fb1b4d38ca316f8a9b1be0bfcf1
Mclakes <lekanhayo@gmail.com>
2023-01-04 00:15:26 +0100

    videoDetails

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect} from 'react';
+import ReactPlayer from 'react-player';
+import { Typography, Box, Stack} from '@mui/material'
+import { CheckCircle  } from '@mui/icons-material';
+
+import { Video} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { useParams } from 'react-router-dom';
+
 
 const VideoDetail = () => {
+  const [videoDetail, setVideosDetail] = useState(null);
+  const { id } = useParams();
+
+  useEffect(()=> {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then((data) => setVideosDetail(data.items[0]))
+  }, [id]);
+
+  // const 
+
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack direction= {{xs: 'column', md: 'row'}}>
+        <Box flex={1}>
+          <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {videoDetail.snippet.title}
+            </Typography>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
-export default VideoDetail
+export default VideoDetail;
