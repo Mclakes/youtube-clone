ac43d009ccf12b6a3b842a5b60fe8767b948d770
Mclakes <lekanhayo@gmail.com>
2023-01-04 11:34:08 +0100

    snipppet error

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,9 +4,9 @@ import ReactPlayer from 'react-player';
 import { Typography, Box, Stack} from '@mui/material'
 import { CheckCircle  } from '@mui/icons-material';
 
-import { Video} from './';
+import { Video } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 
 const VideoDetail = () => {
@@ -15,11 +15,12 @@ const VideoDetail = () => {
 
   useEffect(()=> {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
-      .then((data) => setVideosDetail(data.items[0]))
+      .then((data) => {setVideosDetail(data.items[0])})
   }, [id]);
 
-  // const 
+  if(!videoDetail?.snippet) return 'Loading...';
 
+  const { snippet : { title, channelId, channelTitle }, statistic : { viewCount, likeCount} } = videoDetail;
 
   return (
     <Box minHeight="95vh">
@@ -28,8 +29,24 @@ const VideoDetail = () => {
           <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
-              {videoDetail.snippet.title}
+              {title}
             </Typography>
+            <Stack direction="row" justifyContent="space-between" sx={{color: "#fff"}} py={1} px={2}>
+              <Link to={`/channel/${channelId}`}>
+                <Typography variant={{ sm: "subtitle1", md: 'h6'}} color="#fff">
+                  {channelTitle}
+                  <CheckCircle sx={{ fontSize: '12px', color: 'gray', ml: '5px'}}/>
+                </Typography>
+              </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography variant="body1" sx={{ opacity: '0.7'}}>
+                  {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+                <Typography variant="body1" sx={{ opacity: '0.7'}}>
+                  {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
+            </Stack>
           </Box>
         </Box>
       </Stack>
